d5900e0978fc66402bf1d60d380185eab7b3b887
elvisrfarias <elvisrfarias1@gmail.com>
2025-10-25 12:03:48 -0300

    feat: Ajustado formatação

--- src/components/AvatarCustom.tsx
+++ src/components/AvatarCustom.tsx
@@ -1,15 +1,14 @@
-'use client';
+"use client";
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
-
 const AvatarCustom = () => {
-	return (
-		<Avatar>
-			<AvatarImage src="./assets/img-athus.png" />
-			<AvatarFallback>Athus - Concórdia</AvatarFallback>
-		</Avatar>
-	)
-}
+  return (
+    <Avatar>
+      <AvatarImage src="./assets/img-athus.png" />
+      <AvatarFallback>Athus - Concórdia</AvatarFallback>
+    </Avatar>
+  );
+};
 
 export default AvatarCustom;
